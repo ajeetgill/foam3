5d2d44a516a1a8348f3567579657c30df3afb76c
Ruby <32865869+noodlemoodle@users.noreply.github.com>
2024-11-23 21:37:35 -0800

    update loginview (#3585)
    * update loginview
    
    * fix resetpassword visibility
    
    * add signInWithOIDC to clientloginservice
    
    * use loginservice signinwithoidc
    
    * fix  actionview
    
    * fix login on enter

--- src/foam/nanos/auth/login/LoginView.js
+++ src/foam/nanos/auth/login/LoginView.js
@@ -13,18 +13,16 @@ foam.CLASS({
     'appConfig',
     'clientLoginService',
     'loginVariables',
-    'memento',
     'stack',
-    'theme',
-    'displayWidth?',
-    'loginSuccess'
+    'logAnalyticEvent',
+    'currentMenu?',
+    'params'
   ],
 
   requires: [
+    'foam.nanos.app.AppBadgeView',
     'foam.nanos.auth.login.SignIn',
     'foam.nanos.auth.login.SignUp',
-    'foam.u2.Element',
-    'foam.u2.borders.SplitScreenGridBorder',
     'foam.u2.stack.StackBlock'
   ],
 
@@ -33,50 +31,21 @@ foam.CLASS({
     width: 100%;
   }
 
-  /* ON RIGHT SIDE ALL **** */
-  ^ .centerVertical {
-    padding-top: 3vh;
-    max-width: 30vw;
-    margin: 0 auto;
-  }
-
 
   .foam-u2-dialog-ApplicationPopup ^content-form {
     width: 100%;
     padding: 2vw 0;
   }
-  .foam-u2-dialog-ApplicationPopup ^ .centerVertical {
-    max-width: 100vw;
-  }
-
-  /* SET ABOVE DATA */
-  ^.topBar-logo-Back {
-    display: none;
-  }
-
-  /* SET ON LOGO IMG */
-  ^ .logoCenterVertical {
-    margin: 0 auto;
-    text-align: center;
-    display: block;
-  }
-  ^ .top-bar-img {
-    height: 4vh;
-  }
-
-  /* TITLE TXT ON DATA */
-  ^ .title-top {
-    font-size: 2.5em;
-    padding-top: 2vh;
-    font-weight: bold;
-  }
 
   /* ON DATA */
   ^content-form {
-    align-self: center;
-    width: 75%;
-    padding: 2vw;
+    width: 100%;
     box-sizing: border-box;
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    align-self: center;
+    padding: 2rem;
   }
 
   /* ON ALL FOOTER TEXT */
@@ -90,69 +59,27 @@ foam.CLASS({
     text-align: end;
   }
 
+  ^disclaimer {
+    text-align: center;
+  }
+
+  ^tc-link {
+    background: none;
+    border: 1px solid transparent;
+    color: $primary400;
+    text-decoration: none;
+  }
+  ^legal {
+    position: absolute;
+    bottom: 1.2rem;
+    margin: 0 1rem;
+    text-align: center;
+    font-size: 0.8rem;
+    width: 100%;
+  }
+
   ^center-footer > ^signupLink {
     margin-bottom: 2rem;
-  }
-
-  /* TOP-TOP BAR NAV to go with backLink_ */
-  ^ .top-bar-nav {
-    background: /*%LOGOBACKGROUNDCOLOUR%*/ #202341;
-    width: 100%;
-    height: 4vh;
-    border-bottom: solid 1px #e2e2e3
-  }
-  /* ON TXT IN TOP-TOP NAV */
-  ^ .topBar-txt-link {
-    cursor: pointer;
-    font-size: 2.5vh;
-    font-weight: normal;
-    font-style: normal;
-    font-stretch: normal;
-    letter-spacing: normal;
-    color: #8e9090;
-    margin-left: 2vw;
-    margin-top: 1vw;
-  }
-
-  /* DISCLAIMER */
-      /* ON NO IMG SPLIT & IMG SPLIT */
-  ^ .disclaimer-login {
-    width: 35vw;
-    font-size: 0.75em;
-    color: #8e9090;
-    margin-left: 12vw;
-    line-height: 1.5;
-    background: transparent;
-  }
-
-/* ON LEFT SIDE IMG */
-  ^ .cover-img-block1 {
-    /* align img with disclaimer */
-    display: flex;
-    flex-direction: column;
-    flex-wrap: nowrap;
-    align-items: center;
-  }
-  ^image-one {
-    width: 48vw;
-    padding-top: 25px;
-  }
-  
-  @media (min-width: /*%DISPLAYWIDTH.MD%*/ 786px ) {
-    .foam-nanos-auth-login-LoginView .foam-u2-borders-SplitScreenGridBorder {
-      padding: 0 4vw;
-    }
-  }
-  @media (min-width: /*%DISPLAYWIDTH.LG%*/ 960px ) {
-    .topBar-logo-Back {
-      display: flex;
-      justify-content: center;
-      height: 6vh;
-      background: /*%LOGOBACKGROUNDCOLOUR%*/ #202341;
-    }
-    .foam-nanos-auth-login-LoginView-image-one {
-      width: 28vw;
-    }
   }
   `,
 
@@ -163,22 +90,7 @@ foam.CLASS({
 
   properties: [
     {
-      class: 'Boolean',
-      name: 'topBarShow_',
-      factory: function() {
-        return !! this.backLink_;
-      },
-      hidden: true
-    },
-    {
       name: 'data',
-      factory: () => {
-       return {};
-      },
-      view: { class: 'foam.u2.detail.VerticalDetailView' }
-    },
-    {
-      name: 'view',
       factory: () => {
        return {};
       },
@@ -197,40 +109,6 @@ foam.CLASS({
     },
     {
       class: 'String',
-      name: 'imgPath',
-      expression: function(loginVariables) {
-        return loginVariables.imgPath || '';
-      }
-    },
-    {
-      class: 'foam.u2.ViewSpec',
-      name: 'leftView',
-      documentation: 'Allows using U2 views as left half of the login page, takes precedence over imgPath'
-    },
-    {
-      class: 'String',
-      name: 'backLinkTxt_',
-      factory: function() {
-        let temp = this.backLink_.includes('www.') ?
-          this.backLink_.substring(this.backLink_.indexOf('www.') + 4) :
-          this.backLink_;
-        return temp.includes('://') ?
-          temp.substring(temp.indexOf('://') + 3) :
-          temp;
-      }
-    },
-    {
-      class: 'String',
-      name: 'backLink_',
-      factory: function() {
-        return this.data.backLink_ || this.appConfig.externalUrl || undefined;
-      },
-      hidden: true
-    },
-    { class: 'Boolean', name: 'shouldResize' },
-    { class: 'Boolean', name: 'fullScreenLoginImage' },
-    {
-      class: 'String',
       name: 'modelCls_',
       documentation: `
         If modelCls_ is provided, the data can be created directly from this instead of mode
@@ -243,7 +121,6 @@ foam.CLASS({
         }
       }
     },
-    { class: 'Boolean', name: 'showLogo', value: true },
     { class: 'Boolean', name: 'showTitle', value: true },
     { class: 'Boolean', name: 'showAction', value: true }
   ],
@@ -251,10 +128,19 @@ foam.CLASS({
   messages: [
     { name: 'GO_BACK', message: 'Go to ' },
     { name: 'SWITCH_TO_SIGN_UP_TXT', message: 'Not a user yet?' },
-    { name: 'SWITCH_TO_SIGN_IN_TXT', message: 'Already have an account?' }
+    { name: 'SWITCH_TO_SIGN_IN_TXT', message: 'Already have an account?' },
+    { name: 'DISCLAIMER_TEXT', message: 'By signing up, you accept our ' }
   ],
 
   methods: [
+    {
+      name: 'detach',
+      code: function() {
+        this.SUPER();
+        this.data.detach();
+      }
+    },
+
     function configData() {
       // Use passed in values or default loginVariables defined on ApplicationControllers
       this.param = Object.assign(this.loginVariables, this.param);
@@ -272,131 +158,118 @@ foam.CLASS({
     function render() {
       this.SUPER();
       var self = this;
-
-      this.document.addEventListener('keyup', this.onKeyPressed);
-      this.onDetach(() => {
-        this.document.removeEventListener('keyup', this.onKeyPressed);
-      });
-      let logo = self.imgPath || (this.theme.largeLogo ? this.theme.largeLogo : this.theme.logo);
-
+      if ( this.currentMenu ) {
+        this.logAnalyticEvent({ name: "VIEW_LOAD_LoginView_" + this.currentMenu.id });
+      } else {
+        this.logAnalyticEvent({ name: "VIEW_LOAD_LoginView" });
+      }
       // CREATE DATA VIEW
-      var right = this.E()
-      // Header on-top of rendering data
-        .start()
-          .add(
-            this.slot(function(shouldResize) {
-              return self.E().show( self.showLogo && ( shouldResize || self.fullScreenLoginImage || ! self.imgPath ) )
-              .addClass('topBar-logo-Back')
-              .start('img')
-                .attr('src', logo)
-                .addClass('top-bar-img')
-              .end(); 
-          }))
-        .end()
+      this
+        .addClass(self.myClass())
         // Title txt and Data
-        .callIf(self.showTitle, function() { this.start().addClass('title-top').add(self.data.TITLE).end(); })
+        .callIf(self.showTitle, function() { this.start().addClass('h300').add(self.data.TITLE).end(); })
         .addClass(self.myClass('content-form'))
-        .callIf(self.displayWidth, function() { this.onDetach(self.displayWidth$.sub(self.resize)); })
-        .startContext({ data: self }).tag(self.DATA).endContext()
-        .start()
-          .addClass('align-end')
-          .startContext({ data: self })
-            .callIf(self.mode_ == self.SIGN_IN && self.showAction, function() { this.tag(self.RESET_PASSWORD); })
-          .endContext()
-        .end()
-        .br()
-        .add(
-          this.slot(function(showAction, mode_) {
-            self.configData();
-            return self.E().callIf(showAction, function() {
-              this
-                .startContext({ data: self })
-                  .callIfElse(
-                    self.mode_ == self.SIGN_IN,
-                    function() { this.tag(self.SIGN_IN_ACTION); },
-                    function() { this.tag(self.SIGN_UP_ACTION); }
-                  )
-                .endContext()
-                .br()
-                .br()
-                .start()
+        .start('form')
+          .setID('login')
+          .startContext({ data: this }).tag(this.DATA).endContext()
+        // signin signup action
+          .start()
+            .add(this.slot(function(showAction, mode_) {
+              return self.E().callIf(showAction, function() {
+                this
                   .startContext({ data: self })
-                  .addClass(self.myClass('center-footer'))
-                  // first footer
+                    .start().addClass('align-end').callIf(mode_ == self.SIGN_IN && showAction, function() { this.start(self.RESET_PASSWORD)
+                      .attr('type', 'button').end(); }).end()
+                    .callIfElse(
+                      mode_ == self.SIGN_IN,
+                      function() { this.start(self.SIGN_IN_ACTION).attrs({ type: 'submit', form: 'login' }).end(); },
+                      function() { this.start(self.SIGN_UP_ACTION).attrs({ type: 'submit', form: 'login' }).end(); }
+                    )
+                  .endContext();
+              });
+            }))
+          .end()
+        .end()
+        .start().style({ display: 'contents' })
+          .callIf(self.data.oidcProviderDAO, function() {
+            this.
+              select(self.data.oidcProviderDAO, function(provider) {
+                if ( ! provider ) return;
+                let action = self.Action.create({
+                  name: 'signIn',
+                  label: provider.description,
+                  code: async function () {
+                    await self.clientLoginService.signInWithOIDC(provider)
+                  }
+                });
+    
+                return self.E().style({ display: "contents" }).startContext({ data: self.data }).add(action).endContext();
+              });
+          })
+        .end()
+        .add(
+          this.slot(function(showAction, data$disclaimer, appConfig, mode_) {
+            self.configData();
+            var disclaimer = self.E().style({ display: 'contents' }).callIf(data$disclaimer && appConfig, function() {
+              this.start()
+                .addClass(self.myClass('disclaimer'))
+                .add(self.DISCLAIMER_TEXT)
+                .start('a')
+                  .addClasses([self.myClass('tc-link'), 'h600'])
+                  .add(appConfig.termsAndCondLabel)
+                  .attrs({
+                    href: appConfig.termsAndCondLink,
+                    target: '_blank'
+                  })
+                .end()
+                .add(' and ')
+                .start('a')
+                  .addClasses([self.myClass('tc-link'), 'h600'])
+                  .add(appConfig.privacy)
+                  .attrs({
+                    href: appConfig.privacyUrl,
+                    target: '_blank'
+                  })
+                .end()
+              .end();
+            });
+
+            return self.E().style({ display: 'contents' }).callIfElse(showAction, 
+              function() {
+                this
                   .start()
-                    .addClass(self.myClass('signupLink'))
-                    .start('span')
-                      .addClass('text-with-pad')
-                      .callIfElse(
-                        self.mode_ == self.SIGN_IN,
-                        function() { this.add(self.SWITCH_TO_SIGN_UP_TXT); },
-                        function() { this.add(self.SWITCH_TO_SIGN_IN_TXT); }
-                      )
+                    .startContext({ data: self })
+                    .addClass(self.myClass('center-footer'))
+                    // first footer
+                    .start()
+                      .addClass(self.myClass('signupLink'))
+                      .start('span')
+                        .addClass('text-with-pad')
+                        .callIfElse(
+                          self.mode_ == self.SIGN_IN,
+                          function() { this.add(self.SWITCH_TO_SIGN_UP_TXT); },
+                          function() { this.add(self.SWITCH_TO_SIGN_IN_TXT); }
+                        )
+                      .end()
+                      .start('span')
+                        .callIfElse(
+                          self.mode_ == self.SIGN_IN,
+                          function() { this.add(self.SWITCH_TO_SIGN_UP); },
+                          function() { this.add(self.SWITCH_TO_SIGN_IN); }
+                        )
+                      .end()
                     .end()
-                    .start('span')
-                      .callIfElse(
-                        self.mode_ == self.SIGN_IN,
-                        function() { this.add(self.SWITCH_TO_SIGN_UP); },
-                        function() { this.add(self.SWITCH_TO_SIGN_IN); }
-                      )
-                    .end()
-                  .end()
-                  .endContext()
-                .end();
+                    .endContext()
+                  .end();
+              },
+              function() {
+                this.start().add(disclaimer).end()
+              }
+            ).callIf(showAction, function () {
+              this.tag(self.AppBadgeView, {isReferral: self.data.referralToken || self.params['utm_id']})
             });
           })
         );
-
-      // CREATE SPLIT VIEW
-      if ( this.imgPath || this.leftView ) {
-        var split = this.SplitScreenGridBorder.create();
-        split.rightPanel.add(right);
-      } else {
-        right.addClass('centerVertical').start().addClass('disclaimer-login').add(this.data.DISCLAIMER).end();
-      }
-
-      // RENDER EVERYTHING ONTO PAGE
-      this.addClass()
-      // full width bar with navigation to app landing page
-        .start().addClass('top-bar-nav').show(this.topBarShow_)
-          .start()
-            .start().addClass('topBar-txt-link')
-              .start('span')
-                .addClass('horizontal-flip')
-                .addClass('inline-block')
-                .add('➔')
-              .end()
-              .start('span').add(this.GO_BACK).add(this.backLinkTxt_)
-                .on('click', () => {
-                  window.location = this.backLink_;
-                })
-              .end()
-            .end()
-          .end()
-        .end()
-      // deciding to render half screen with img and data or just centered data
-        .callIfElse( !! (this.imgPath || this.leftView) && !! split, () => {
-          if ( ! this.leftView ) {
-            split.leftPanel
-              .addClass('cover-img-block1')
-              .start('img')
-                .addClass(self.myClass('image-one'))
-                .attr('src', this.imgPath$)
-              .end()
-              .callIf( !! this.data.disclaimer , () => {
-                // add a disclaimer under img
-                split.leftPanel.start('p')
-                  .addClass('disclaimer-login').addClass('disclaimer-login-img')
-                  .add(this.data.DISCLAIMER)
-                .end();
-              });
-          } else {
-            split.leftPanel.tag(this.leftView);
-          }
-          this.add(split);
-        }, function() {
-          this.add(right);
-        });
     }
   ],
 
@@ -450,27 +323,6 @@ foam.CLASS({
             modelOf: 'foam.nanos.auth.RetrievePassword'
           }
         }));
-      }
-    }
-  ],
-
-  listeners: [
-    {
-      name: 'resize',
-      isFramed: true,
-      code: function() {
-        if ( this.displayWidth == 'MD' || this.displayWidth == 'SM' ||this.displayWidth == 'XS' || this.displayWidth == 'XXS' ) {
-          this.shouldResize = true;
-        } else {
-          this.shouldResize = false;
-        }
-      }
-    },
-    function onKeyPressed(e) {
-      e.preventDefault();
-      var key = e.key || e.keyCode;
-      if ( key === 'Enter' || key === 13 ) {
-        this.mode_ == 0 ? this.signInAction(x) : this.signUpAction(x); 
       }
     }
   ]
