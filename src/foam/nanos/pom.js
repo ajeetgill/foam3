ad014225e91b5166050077abd3f173b2488aef74
Adam Van Ymeren <adam@vany.ca>
2025-01-14 06:50:59 -0800

    Load some test classes in js mode too so that the test table view works. (#3655)

--- src/foam/nanos/pom.js
+++ src/foam/nanos/pom.js
@@ -431,7 +431,7 @@ foam.POM({
     { name: "pool/ThreadPoolAgency",                                                      flags: "js|java" },
     { name: "pool/Throttle",                                                              flags: "js|java" },
     { name: "pool/ThrottledAgency",                                                       flags: "js|java" },
-    { name: "pool/test/ThrottledAgencyTest",                                              flags: "java" },
+    { name: "pool/test/ThrottledAgencyTest",                                              flags: "js|java" },
     { name: "auth/PMAuthService",                                                         flags: "js|java" },
     { name: "notification/ClientResendNotificationService",                               flags: "js|java" },
     { name: "notification/DAONotificationRuleAction",                                     flags: "js|java" },
