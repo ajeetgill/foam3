2033c1b35dab6c0a6934cfc08e8ad55afc081e20
Kevin Greer <kgrgreer@gmail.com>
2025-02-10 15:16:04 -0400

    Update migration script.

--- tools/migrate.bash
+++ tools/migrate.bash
@@ -14,10 +14,17 @@ find ./ -type f \( \
      -name "*.md" \) \
    -exec perl -p -i -e 's/foam\.core/foam\.lang/g' \{} \; \
    -exec perl -p -i -e 's/foam\/core/foam\/lang/g' \{} \; \
-   -exec perl -p -i -e 's/NSpec/COREService/g'   \{} \; \
-   -exec perl -p -i -e 's/nanos/core/g'          \{} \; \
-   -exec perl -p -i -e 's/NANOS/CORE/g'          \{} \;
+   -exec perl -p -i -e 's/NSpec/COREService/g'     \{} \; \
+   -exec perl -p -i -e 's/nSpec/service/g'         \{} \; \
+   -exec perl -p -i -e 's/nanos/core/g'            \{} \; \
+   -exec perl -p -i -e 's/NANOS/CORE/g'            \{} \;
 
 cd foam3/src/foam
 git mv core  lang
 git mv nanos core
+
+cd core
+git mv NSpec.js          COREService.js
+git mv NSpecAware.js     COREServiceAware.js
+git mv NSpecFactory.java COREServiceFactory.java
+git mv DAONSpecMenu.js   DAOCOREServiceMenu.js
