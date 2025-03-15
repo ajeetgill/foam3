7b660e6dc838b3fc0eae02e130180d01318e2ab9
Joel Hughes <joel@nanopay.net>
2025-03-15 17:33:56 -0400

    CopyMaker - support source directory as well.

--- tools/CopyMaker.js
+++ tools/CopyMaker.js
@@ -12,17 +12,24 @@ const fs_   = require('fs');
 const path_ = require('path');
 const b_    = require('./buildlib');
 
+var defaultTargetDir = 'journals';
+
 exports.visitPOM = function(pom) {
   pom.copy && pom.copy.forEach(fn => {
     var source     = path_.resolve(foam.cwd, fn.source);
     var i          = source.lastIndexOf('/');
     var sourceName = source.substring(i+1);
 
-    var targetDir  = fn.targetDir || 'journals';
+    var targetDir  = fn.targetDir || defaultTargetDir;
     var target     = X.builddir + '/' + targetDir + '/' + sourceName;
 
-    console.log(`[Copy Maker] Copying File ${source} to ${target}`);
-    b_.ensureDir(path_.dirname(target));
-    b_.copyFile(source, target);
+    if ( fs_.lstatSync(source).isDirectory() ) {
+      console.log(`[Copy Maker] Copying Directory ${source} to ${targetDir}`);
+      b_.copyDir(source, targetDir);
+    } else {
+      console.log(`[Copy Maker] Copying File ${source} to ${target}`);
+      b_.ensureDir(path_.dirname(target));
+      b_.copyFile(source, target);
+    }
   });
 }
