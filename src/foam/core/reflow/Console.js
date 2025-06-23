806cd2c2f094a2a8daa2bcfac978ddb89c8b8e19
Kevin Greer <kgrgreer@gmail.com>
2025-06-23 08:21:29 -0300

    Refactor/simplify the way Console and Flow objects interact to save Flows. The logic was moved out of Flows and into Console where it belonged. Fixes one undo/redo bug.

--- src/foam/core/reflow/Console.js
+++ src/foam/core/reflow/Console.js
@@ -152,9 +152,9 @@ foam.CLASS({
           .end()
 
           .start().addClass(this.myClass('header-actions'))
-            .startContext({ data: this.data.value.mementoMgr })
-              .tag(this.data.value.mementoMgr.BACK)
-              .tag(this.data.value.mementoMgr.FORTH)
+            .startContext({ data: this.data.mementoMgr })
+              .tag(this.data.mementoMgr.BACK)
+              .tag(this.data.mementoMgr.FORTH)
             .endContext()
             .startContext({data: this})
               .tag(this.CANCEL)
@@ -247,7 +247,7 @@ foam.CLASS({
         var flow = this.data.value;
 
         flow.name     = '';
-        flow.mementoMgr.clear();
+        this.mementoMgr.clear();
         flow.version  = undefined;
         flow.revision = undefined;
       }
@@ -363,19 +363,21 @@ foam.CLASS({
           .endContext()
         .end();
     },
+
     function renderOpened(e) {
-        e.start().addClass(this.myClass('left-container'))
-          .start().addClass(this.myClass('left-header'))
-            .start('span').add('Contents').end()
-            .startContext({ data: this })
-              .tag(this.MENU_CONTROL)
-            .endContext()
-          .end()
-          .start('table')
-            .attr('cellpadding', '4')
-            .call(this.branch, [this, this.data, 0])
+      e.start().addClass(this.myClass('left-container'))
+        .start().addClass(this.myClass('left-header'))
+          .start('span').add('Contents').end()
+          .startContext({ data: this })
+            .tag(this.MENU_CONTROL)
+          .endContext()
         .end()
+        .start('table')
+          .attr('cellpadding', '4')
+          .call(this.branch, [this, this.data, 0])
+        .end();
     },
+
     function render() {
       var self = this;
       this.addClass();
@@ -929,6 +931,13 @@ foam.CLASS({
       name: 'value',
       // The Console's Flow Value, which is the Flow object it is saved as
       factory: function() { return this.Flow.create(); }
+    },
+    {
+      class: 'FObjectProperty',
+      name: 'mementoMgr',
+      factory: function() {
+        return foam.memento.MementoMgr.create({memento$: this.value.script$, position$: this.value.revision$});
+      }
     }
   ],
 
@@ -980,43 +989,17 @@ foam.CLASS({
         }
       });
 
-      var feedback_ = false;
+      // If this.value.script changes
+      //   update this.flowChildren
+      //   clearFlow()
+      //   rebuild flow
+      // If flowChildren changes
+      //   update this.value.script
 
-      this.flowChildren$.sub(() => {
-        if ( feedback_ ) return;
-        feedback_ = true;
-        try {
-          this.value.memento = this.flowChildren;
-        } finally {
-          feedback_ = false;
-        }
-        });
-      this.value.memento$.sub(async () => {
-        if ( feedback_ ) return;
-        feedback_ = true;
-        try {
-          var cs = this.value.memento;
-          var currentBlockName = this.selected ? this.selected.flowName : this.flowName;
-          this.clearFlow();
-          for ( var i = 0 ; i < cs.length ; i++ ) {
-            var c = cs[i];
+      this.value.script$.sub(this.onScriptChange);
 
-            this.eval_(c.cmd);
+      this.flowChildren$.sub(this.onFlowChildrenChange);
 
-            // TODO: await
-            this.currentBlock.flowName = c.flowName;
-            if ( this.currentBlock.value && c.value ) {
-              this.currentBlock.value.copyFrom(c.value);
-            }
-
-            await this.currentBlock.value?.onLoad?.();
-          }
-          this.selected = currentBlockName == this.flowName ? this : this.findFlowChildByName(currentBlockName);
-        } finally {
-          feedback_ = false;
-        }
-      });
-//      this.value.memento$.follow(this.flowChildren$);
 
       var layout = this.start(this.Layout);
 
@@ -1093,7 +1076,6 @@ foam.CLASS({
     },
 
     function log(...args) {
-      debugger;
       this.currentBlock.log.apply(this.currentBlock, args);
     },
 
@@ -1249,9 +1231,9 @@ foam.CLASS({
       // will make it easy to implement undo/redo in the future.
       var flow = this.value;
 
-      flow.MEMENTO.postSet.call(this, this.menento, this.memento);
+      this.generateScript();
       flow.version++;
-      flow.mementoMgr.clear();
+      this.mementoMgr.clear();
       flow.flowDAO.put(this.value).then(ret => this.value.copyFrom(ret));
     },
 
@@ -1261,6 +1243,19 @@ foam.CLASS({
       } else {
         this.selected = this.flowChildren[i];
       }
+    },
+
+    function generateScript() {
+      var json = foam.json.Outputter.create({
+        pretty: true,
+        strict: true,
+        formatDatesAsNumbers: false,
+        outputDefaultValues: false,
+        useShortNames: false,
+        propertyPredicate: function(_, p) { return p.name === 'reactions_' || ( ! p.externalTransient && ! p.networkTransient ); }
+      });
+
+      this.value.script = json.stringify(this.flowChildren);
     }
   ],
 
@@ -1363,6 +1358,51 @@ foam.CLASS({
       isMerged: true,
       mergeDelay: 600,
       code: function() { this.input_.focus(); }
+    },
+    {
+      name: 'onScriptChange',
+      code: async function() {
+        if ( this.feedback_ ) return;
+        this.feedback_ = true;
+        try {
+          var script = this.value.script;
+
+          var cs = script ? foam.json.parseString(script, this.__subContext__) : [];
+          var currentBlockName = this.selected ? this.selected.flowName : this.flowName;
+
+          this.clearFlow();
+
+          for ( var i = 0 ; i < cs.length ; i++ ) {
+            var c = cs[i];
+
+            this.eval_(c.cmd);
+
+            this.currentBlock.flowName = c.flowName;
+
+            if ( this.currentBlock.value && c.value ) {
+              this.currentBlock.value.copyFrom(c.value);
+            }
+
+            await this.currentBlock.value?.onLoad?.();
+          }
+
+          this.selected = currentBlockName == this.flowName ? this : this.findFlowChildByName(currentBlockName) || this;
+        } finally {
+          this.feedback_ = false;
+        }
+      }
+    },
+    {
+      name: 'onFlowChildrenChange',
+      isFramed: true,
+      code: async function() {
+        this.feedback_ = true;
+        try {
+          this.generateScript();
+        } finally {
+          this.feedback_ = false;
+        }
+      }
     }
   ]
 });
