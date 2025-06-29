4fdbe2130adb0df80ccb3489b33f37931ecbca40
Kevin Greer <kgrgreer@gmail.com>
2025-06-29 10:11:33 -0300

    Add float TestResult.

--- src/foam/core/reflow/float/Test.js
+++ src/foam/core/reflow/float/Test.js
@@ -26,22 +26,96 @@ foam.CLASS({
 
 foam.CLASS({
   package: 'foam.core.reflow.float',
-  name: 'Test',
+  name: 'TestResults',
+
+  requires: [ 'foam.dao.EasyDAO' ],
+
+  imports: [ 'eval_' ],
 
   properties: [
     {
+      class: 'Int',
+      name: 'passed',
+      visibility: 'RO',
+      transient: true
+    },
+    {
+      class: 'Int',
+      name: 'failed',
+      visibility: 'RO',
+      transient: true
+    },
+    {
+      name: 'dao',
+      hidden: true,
+      transient: true,
+      factory: function() {
+        return this.EasyDAO.create({daoType: 'MDAO', of: foam.core.reflow.float.Test});
+      }
+    }
+  ],
+
+  methods: [
+    function report(test) {
+      this.dao.put(test);
+      if ( test.status ) { this.passed++; } else { this.failed++; }
+      test.status$.sub((_, __, ___, slot) => {
+        if ( slot.get() ) {
+          this.passed++;
+          this.failed--;
+        } else {
+          this.passed--;
+          this.failed++;
+        }
+
+        this.dao.put(test);
+      });
+    }
+  ],
+
+  actions: [
+    {
+      name: 'browse',
+      code: function() {
+        this.eval_('dao(testResults.dao, "Test Results")');
+      }
+    }
+  ]
+});
+
+
+foam.CLASS({
+  package: 'foam.core.reflow.float',
+  name: 'Test',
+
+  tableColumns: [
+    'description', 'status'
+  ],
+
+  properties: [
+    {
+      class: 'Long',
+      name: 'id',
+      reactive: false,
+      hidden: true,
+      transient: true
+    },
+    {
       class: 'String',
       name: 'description',
+      reactive: false,
     },
     {
       class: 'String',
       name: 'notes',
+      reactive: false,
       width: 80,
       view: { class: 'foam.u2.tag.TextArea', rows: 3, cols: 78 }
     },
     {
       class: 'String',
       name: 'received',
+      reactive: false,
       transient: true,
       visibility: 'RO',
       width: 80,
@@ -50,18 +124,19 @@ foam.CLASS({
     {
       class: 'String',
       name: 'expected',
-      visibility: 'RO',
+//      visibility: 'RO',
+      reactive: false,
       width: 80,
       view: { class: 'foam.u2.tag.TextArea', rows: 6, cols: 78 }
     },
     {
       class: 'Boolean',
       name: 'status',
+      reactive: false,
       visibility: 'RO',
       view: 'foam.core.reflow.float.PassFailView',
-      xxxview: {
-        class: 'foam.u2.view.ChoiceView',
-        choices: [ [ false, '<span style="color:red">FAILED</span>' ], [ true, 'PASSED' ] ]
+      tableCellFormatter: function(value, obj) {
+        this.start(foam.core.reflow.float.PassFailView, {data: value});
       },
       expression: function(received, expected) {
         return received === expected;
