99749a8e3b881fc3419a9d01dda5aba8f9ed1bd3
ruby <ruby.zhang@payticconnect.com>
2025-07-25 01:54:10 -0700

    works except groupby

--- src/foam/core/reflow/PivotByView.js
+++ src/foam/core/reflow/PivotByView.js
@@ -76,56 +76,119 @@ foam.CLASS({
   `,
 
   properties: [
-    { name: 'x' },
-    { name: 'y' },
     { name: 'currentHoverCol' },
-    { name: 'currentHoverRow' }
+    { name: 'currentHoverRow' },
+    { name: 'colIndexMap' }
   ],
 
   methods: [
-    
     function render() {
-      var data = this.data;
-      var colsDict = this.makeDictionary(data.cols);
-      var rowsDict = this.makeDictionary(data.rows);
-      console.log(rowsDict);
+      this.colIndexMap = [];
+      var self = this;
       var rowDepth = this.data.yFunc.length;
       var colDepth = this.data.xFunc.length;
-
-      this.addClass();
-
+      var rowsDict = this.makeDictionary(this.data.rows);
+      // instead of using data.cols which might include cols that aren't in the rows data
+      var colsDict = this.getColsFromRows(rowsDict, rowDepth);
       var table = this.start('table').addClass(this.myClass('table'));
-      this.renderColHeaders(table, colsDict, [], 0, rowDepth);
+      this.renderColHeaders(self, table, colsDict, [], 0, rowDepth, colDepth);
+      this.renderRows(self, table, rowsDict, 0, rowDepth);
+      table.end();
     },
 
+    function renderRows(self, table, rows, currDepth, rowDepth, prefix = '') {
+      var rowKeys = Object.keys(rows);
+      if ( currDepth == rowDepth ) {
+        return 1;
+      }
+      var rowSpans = [];
+      var retData = { rowSpan: 1, row: null };
+      for ( var i = 0; i < rowKeys.length; i++ ) {
+        const key = prefix + rowKeys[i];
+        if ( currDepth < rowDepth - 1 ) {
+          ret = renderRows(self, table, rows[rowKeys[i]], currDepth + 1, rowDepth, key);
+          if ( ret.row ) {
+            var el = foam.u2.Element.create({nodeName: 'th'});
+            el.attrs({ 'rowspan' : ret.rowSpan, 'key' : key })
+              .addClass(self.myClass('th'))
+              .add(rowKeys[i].toString())
+              .on('mouseover', () => self.currentHoverRow = key)
+              .on('mouseleave', () => self.currentHoverRow = undefined)
+              .enableClass(self.myClass('highlighted-row'), self.slot((currentHoverRow) => currentHoverRow === key || key.startsWith(currentHoverRow) || currentHoverRow?.startsWith(key) ));
+            ret.row.insertBefore(el, ret.row.children[0]);
+            if ( i == 0 ) retData.row = ret.row;
+          }
+          rowSpans.push(ret.rowSpan);
+        } else {
+          var row = table.start('tr').addClass(self.myClass('tr'))
+          row.start('th')
+            .attrs({ 'key' : key })
+            .addClass(self.myClass('th'))
+            .on('mouseover', () => self.currentHoverRow = key)
+            .on('mouseleave', function() { self.currentHoverCol = undefined; self.currentHoverRow = undefined; })
+            .enableClass(self.myClass('highlighted-col'), self.slot((currentHoverRow) => currentHoverRow === key || key.startsWith(currentHoverRow) || currentHoverRow?.startsWith(key) ))
+            .add(rowKeys[i].toString())
+            .end();
+          var map = self.flattenMap(rows[rowKeys[i]]);
+          row.forEach(self.colIndexMap, function(col) {
+            const c = col;
+            row.start('td')
+              .addClass(self.myClass('td'))
+              .attrs({ 'col' : c, 'row' : key })
+              .on('mouseover', function() { self.currentHoverCol = c; self.currentHoverRow = key; })
+              .on('mouseleave', function() { self.currentHoverCol = undefined; self.currentHoverRow = undefined; })
+              .enableClass(
+                self.myClass('highlighted-col'),
+                self.slot((currentHoverCol, currentHoverRow) => {
+                  var ret = currentHoverCol === c || currentHoverRow === key
+                    || c.startsWith(currentHoverCol) || key.startsWith(currentHoverRow);
+                  return ret;
+                }))
+              .add(map[c] || '')
+            .end();
+          })
+          console.log('map',map);
+          row.end();
+          if ( i == 0 ) retData.row = row;
+          rowSpans.push(1);
+        } 
+      }
+      retData.rowSpan = rowSpans.reduce((x, y) => x + y, 0);
+      return retData;
+    },
 
-    function renderColHeaders(table, cols, rows, depth, rowDepth = 1) {
+    function renderColHeaders(self, table, cols, rows, depth, rowDepth, colDepth, keyPrefix) {
+      if ( ! keyPrefix ) keyPrefix = '';
       var row;
       if ( rows.length < depth + 1 ) {
         row = table.start('tr').addClass(this.myClass('tr'));
-        for ( var i = 0; i < rowDepth; i++ ) {
-          row.start('th').addClass(this.myClass('th')).add("X").end();
-        }
+        if ( depth == 0 ) row.start('th').addClass(this.myClass('th')).attrs({ 'colspan' : rowDepth, 'rowspan' : colDepth }).add('').end();
         rows.push(row);
       }
       row = rows[depth];
-      var keys = Object.keys(cols);
-      var hasChildren = keys.some(c => cols[c] instanceof Object); // sus
+      var keys = Object.keys(cols).sort();
+      var hasChildren = keys.some(c => cols[c] instanceof Object && Object.keys(cols[c]).length);
       var colSpans = [];
       if ( hasChildren ) {
         keys.forEach(c => {
-          childKeys = 
-            this.renderColHeaders(table, cols[c], rows, depth + 1, rowDepth);
-          colSpans.push(childKeys);
+          childKeysSpan = 
+            this.renderColHeaders(self, table, cols[c], rows, depth + 1, rowDepth, colDepth, keyPrefix + c);
+          colSpans.push(childKeysSpan);
         });
       }
       for ( var i = 0; i < keys.length; i++ ) {
         var colSpan = hasChildren ? colSpans[i] : 1;
+        const key = keyPrefix + keys[i];
         rows[depth].start('th')
           .addClass(this.myClass('th'))
-          .attrs({'colspan':colSpan})
+          .attrs({ 'colspan' : colSpan, 'key' : key })
+          .on('mouseover', () => self.currentHoverCol = key)
+          .on('mouseleave', function() { self.currentHoverCol = undefined; self.currentHoverRow = undefined; })
+          .enableClass(self.myClass('highlighted-col'), self.slot((currentHoverCol) => currentHoverCol === key || currentHoverCol?.startsWith(key) || key.startsWith(currentHoverCol)))
           .add(keys[i].toString())
         .end();
+        if ( depth == colDepth - 1 ) this.colIndexMap.push(key);
+
       }
       if ( depth = 0 ) rows.forEach(r => r.end());
       return hasChildren ? 
@@ -133,7 +196,7 @@ foam.CLASS({
         keys.length;
     },
 
-    function makeDictionary(data /** instanceof GroupBy */) {
+    function makeDictionary(data /** GroupBy */) {
       if ( ! foam.mlang.sink.GroupBy.isInstance(data) ) return data.value || data;
       var ret = {};
       var keys = data.sortedKeys();
@@ -141,6 +204,48 @@ foam.CLASS({
         ret[keys[i]] = makeDictionary(data.groups[keys[i]]);
       }
       return ret;
+    },
+
+    function getColsFromRows(rows, depth) {
+      var cols = {};
+      this.getColsHelper(rows, 0, cols, depth);
+      return cols;
+    },
+    function getColsHelper(rows, currDepth, cols, depth) {
+      if ( currDepth === depth ) {
+        for ( var key in rows ) {
+          if ( ! cols[key] ) cols[key] = {};
+          this.merge(cols[key], rows[key]);
+        }
+      } else if ( rows && typeof rows === 'object' && Object.keys(rows).length ) {
+        for ( var key in rows ) {
+          this.getColsHelper(rows[key], currDepth + 1, cols, depth);
+        }
+      }
+    },
+  
+    function merge(ret, toMerge) {
+      for ( var key in toMerge ) {
+        if ( typeof toMerge[key] === 'object' && toMerge[key] !== null ) {
+          if ( ! ret[key] ) ret[key] = {};
+          this.merge(ret[key], toMerge[key]);
+        } else {
+          ret[key] = toMerge[key];
+        }
+      }
+    },
+
+    function flattenMap(obj, prefix = '') {
+      var map = {};
+      for (var key in obj) {
+        var val = obj[key];
+        if (typeof val === 'object' && val !== null) {
+          Object.assign(map, flattenMap(val, prefix + key));
+        } else {
+          map[prefix + key] = val;
+        }
+      }
+      return map;
     }
   ]
 });
