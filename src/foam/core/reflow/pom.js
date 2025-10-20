ad4d446c66d2c27fd516b526cc0fa568f9a66a81
Abdelaziz Mahdy <Abdelaziz.h.mahdy@gmail.com>
2025-10-20 18:05:48 -0300

    fix(pom): remove unused CellsSink dependency from POM configuration

--- src/foam/core/reflow/pom.js
+++ src/foam/core/reflow/pom.js
@@ -9,7 +9,6 @@ foam.POM({
     { name: '../../u2/mlang/Pie',      flags: 'js|java' },
     { name: 'AbstractDAOAgent',        flags: 'js' },
     { name: 'DAOResolverMixin',        flags: 'js' },
-    { name: 'CellsSink',               flags: 'js|java' },
     { name: 'Check',                   flags: 'js' },
     { name: 'CitationSink',            flags: 'js|java' },
     { name: 'Clock',                   flags: 'js' },
