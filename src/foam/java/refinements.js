40ebc196b03d409a9c3a54c4902ee8bc4854f632
Kevin Greer <kgrgreer@gmail.com>
2025-10-22 13:55:29 -0300

    Revert "Remove Date specific javaAdapt code from DateTime java Properties."
    This reverts commit 44341751b6e6c71565b2a1adc7e51323c6a065f1.

--- src/foam/java/refinements.js
+++ src/foam/java/refinements.js
@@ -1489,8 +1489,7 @@ foam.CLASS({
   properties: [
     ['javaInfoType',    'foam.lang.AbstractDatePropertyInfo'],
     ['javaJSONParser',  'foam.lib.json.DateParser.instance()'],
-    ['sqlType',         'TIMESTAMP WITHOUT TIME ZONE'],
-    ['javaAdapt',       '']
+    ['sqlType',         'TIMESTAMP WITHOUT TIME ZONE']
   ],
 
   methods: [
@@ -1531,8 +1530,7 @@ foam.CLASS({
      `
       // convert the Date to be noon in GMT
       val = new java.util.Date(val.getTime() / 86400000l * 86400000l + 43200000l);
-     `
-    ]
+     ` ]
   ],
 
    methods: [
